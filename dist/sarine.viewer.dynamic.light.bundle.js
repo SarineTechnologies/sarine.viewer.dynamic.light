8a10bd89f5dbea00dccf231be9c15066a94cd246
unknown <Adic@AdiC-pc.sarin.local>
2015-03-25 17:38:59 +0200

    bla

--- dist/sarine.viewer.dynamic.light.bundle.js
+++ dist/sarine.viewer.dynamic.light.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.dynamic.light - v0.0.6 -  Tuesday, March 24th, 2015, 12:11:43 PM 
+sarine.viewer.dynamic.light - v0.0.7 -  Wednesday, March 25th, 2015, 5:38:48 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -59,8 +59,8 @@ sarine.viewer.dynamic.light - v0.0.6 -  Tuesday, March 24th, 2015, 12:11:43 PM
       return rm.loadImage.apply(this, [src]);
     };
 
-    Viewer.prototype.setTimeout = function(fun, delay) {
-      return rm.setTimeout.apply(this, [this.delay]);
+    Viewer.prototype.setTimeout = function(delay, callback) {
+      return rm.setTimeout.apply(this, [this.delay, callback]);
     };
 
     return Viewer;
@@ -94,7 +94,7 @@ sarine.viewer.dynamic.light - v0.0.6 -  Tuesday, March 24th, 2015, 12:11:43 PM
       this.nextImage.apply(this);
       if (this.playing) {
         _t = this;
-        return this.setTimeout(this.delay).then(_t.play);
+        return this.setTimeout(this.delay, _t.play);
       }
     };
 
